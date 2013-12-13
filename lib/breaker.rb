b91c7c9f04c59415b5bc807fafac4999776f9ab6
ahawkins <me@broadcastingadam.com>
2013-12-14 00:22:05 +0100

    Trip in open state

--- lib/breaker.rb
+++ lib/breaker.rb
@@ -1,5 +1,46 @@
 require "breaker/version"
 
 module Breaker
-  # Your code goes here...
+  CircuitOpenError = Class.new RuntimeError
+
+  class Circuit
+    attr_accessor :failure_threshold
+
+    def initialize(options = {})
+      options.each_pair do |key, value|
+        send "#{key}=", value
+      end
+
+      @state = :closed
+      @failure_count = 0
+    end
+
+    def open?
+      @state == :open
+    end
+
+    def closed?
+      @state == :closed
+    end
+
+    def run
+      raise CircuitOpenError if open?
+
+      begin
+        yield
+      rescue => ex
+        failed
+        raise ex
+      end
+    end
+
+    private
+    def failed
+      @failure_count = @failure_count + 1
+
+      if @failure_count >= @failure_threshold
+        @state = :open
+      end
+    end
+  end
 end
--- test/truth_test.rb
+++ test/truth_test.rb
@@ -1,7 +1,29 @@
 require_relative 'test_helper'
 
-class TruthTest < MiniTest::Unit::TestCase
-  def test_truth
-    assert true
+class AcceptanceTest < MiniTest::Unit::TestCase
+  DummyError = Class.new RuntimeError
+
+  def test_goes_into_open_state_when_failure_threshold_reached
+    breaker = Breaker::Circuit.new failure_threshold: 3
+
+    assert breaker.closed?
+
+    3.times do
+      begin
+        breaker.run do
+          raise DummyError
+        end
+      rescue DummyError
+        # all good, this is intended
+      end
+    end
+
+    assert_raises Breaker::CircuitOpenError do
+      breaker.run do
+        1 + 1
+      end
+    end
+
+    assert breaker.open?
   end
 end
